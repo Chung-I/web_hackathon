dbafd25b927267a18da698e77b73f43efc2ad91d
Chung-I <leon129506@gmail.com>
2016-11-23 21:55:59 +0800

    school department added

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -29,11 +29,19 @@ class ArticlePage extends Component {
   }*/
   render() {
     let content = this.state.article.content ? this.state.article.content : '';
-    content = content.replace(/\n/g, "<br />");
-    content = {__html: content};
+    content = content.split(/\n/);
     return (
       <div>
-        <p dangerouslySetInnerHTML={content} />
+        {content.map(line => {
+            if(line.match(/http.*jpg/)) {
+              console.log(line);
+              line = line.replace(/(http.*jpg)/, "<img className=\"img-fluid\" src=\"$1\" height=\"100px\" width=\"100px\" >");
+              line = {__html: line};
+              return (<p dangerouslySetInnerHTML={line} />);
+            }
+            else return (<p>{line}</p>);
+          })
+        }
       </div>
     );
   }
--- src/components/BoardPage.js
+++ src/components/BoardPage.js
@@ -61,16 +61,23 @@ class BoardPage extends Component {
       <table className="table table-responsive">
         <thead>
           <tr>
+            <td>文章標題</td>
+            <td>大學</td>
+            <td>科系</td>
           </tr>
         </thead>
         <tbody>
           {this.state.articles.map(article => {
             const forumName = article.forumName.toLowerCase();
+            const school = article.anonymousSchool ? '匿名' : article.school;
+            const department = article.anonymousDepartment ? '匿名' : article.department;
             return (
               <tr>
-                  <td>
-                    <a href={`#/${forumName}/${article.id}`}>{article.title}</a>
-                  </td>
+                <td>
+                  <a href={`#/${forumName}/${article.id}`}>{article.title}</a>
+                </td>
+                <td>{school}</td>
+                <td>{department}</td>
               </tr>
             );
           })}
