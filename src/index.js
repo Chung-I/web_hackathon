a1b8eb8bd48c4f916e28643cf6cd5898ce2e8a83
Oliveck10 <oliveck10@gmail.com>
2017-01-04 13:26:47 +0800

    create test page for coloring result (showcase without data)

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import './bootstrap/css/bootstrap-theme.min.css';
 import HomePage from './components/HomePage';
 import CreateEventPage from './components/CreateEventPage';
 import UserFormFillingPage from './components/UserFormFillingPage';
+import PollResultPage from './components/PollResultPage';
 
 render(
   (<Router history={hashHistory}>
     <Route path="/" component={HomePage} />
     <Route path="/create" component={CreateEventPage} />
     <Route path="/form/:eventName" component={UserFormFillingPage} />
+    <Route path="/result" component={PollResultPage} />
   </Router>),
   document.getElementById('root'),
 );
