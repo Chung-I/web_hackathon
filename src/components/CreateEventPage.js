482c751b014075e914aaeffb32affce63c568ad6
Oliveck10 <oliveck10@gmail.com>
2017-01-16 13:28:33 +0800

    remove duplicated code

--- src/components/CreateEventPage.js
+++ src/components/CreateEventPage.js
@@ -38,16 +38,6 @@ class CreateEventPage extends Component {
     ].join('-');
   };
 
-  yyyymmdd = date => {
-    const mm = date.getMonth() + 1; // getMonth() is zero-based
-    const dd = date.getDate();
-
-    return [date.getFullYear(),
-      (mm > 9 ? '' : '0') + mm,
-      (dd > 9 ? '' : '0') + dd,
-    ].join('-');
-  };
-
   handleEventNameChange = event => {
     this.setState({ eventName: event.target.value });
   }
